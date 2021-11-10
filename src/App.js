c80e08378a7a9d580e7e13f2c10a5f859311ad9c
Tyler McKee <tylermckee71@gmail.com>
2021-11-09 21:53:45 -0600

    init app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import GroceryList from './components/GroceryList'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Grocery List</p>
+        <GroceryList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
