1e236b69b646334ac26d42417b81985f04841c69
Tyler McKee <tylermckee71@gmail.com>
2021-11-09 23:42:33 -0600

    add grocery icon to title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import GroceryList from "./components/GroceryList";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Grocery List</p>
+        <div style={titleStyle}>
+          <p>Grocery List</p>
+          <LocalGroceryStoreIcon fontSize="large" />
+        </div>
         <GroceryList items={[]} />
       </header>
     </div>
   );
 }
+
+const titleStyle = {
+  display: "flex",
+  alignItems: "center",
+};
 
 export default App;
