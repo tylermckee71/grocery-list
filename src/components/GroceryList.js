a21316d377598d4a9aab4b67c0227b7b39fdf827
Tyler McKee <tylermckee71@gmail.com>
2021-11-09 22:43:29 -0600

    add functionality

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TextField from '@mui/material/TextField';
 import ListItem from './ListItem';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 export default function GroceryList() {
-    const items = ['tacos', 'burrito']
+    const [listItems, setListItems] = useState([])
+    const [mainTextInput, setMainTextInput] = useState()
+
+    const addItem = () => {
+        setListItems([mainTextInput, ...listItems])
+        setMainTextInput('')
+    }
+
+    const handleMainInputEnterKeyPress = (e) => {
+        if(e.key === 'Enter') addItem()
+    }
+
     return (
         <div>
-            <TextField id="standard-basic" variant="standard" />
-            {items.map((item,index) => <ListItem item={item} key={`${item}-${index}`} />)}
+            <div style={textInputStyle}>
+                <TextField onKeyPress={handleMainInputEnterKeyPress} value={mainTextInput} onChange={(e) => setMainTextInput(e.target.value)} id="standard-basic" variant="standard" />
+                <AddCircleIcon onClick={addItem}/>
+            </div>
+            {listItems.map((item,index) => <ListItem item={item} key={`${item}-${index}`} />)}
         </div>
     )
 }
+
+const textInputStyle={
+    display: 'flex'
+}
+
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,14 +6,17 @@ import TextField from '@mui/material/TextField';
 
 
 export default function ListItem({item = 'Test Item'}) {
-    const [editActive, setEditActive] = useState()
+    const [editActive, setEditActive] = useState(false)
 
+    const handleDeleteClick = () => {
+        console.log('item deleted')
+    }
 
     return (
         <div style={styles}>
-            {editActive ? <TextField /> : <div style={itemNameStyle}>{item}</div>}
-            <EditIcon />
-            <DeleteIcon />
+            {editActive ?<TextField defaultValue={item} id="standard-basic" variant="standard" />: <div style={itemNameStyle}>{item}</div>}
+            <EditIcon onClick={() => setEditActive(!editActive)} />
+            <DeleteIcon onClick={handleDeleteClick} />
         </div>
     )
 }
@@ -21,7 +24,8 @@ export default function ListItem({item = 'Test Item'}) {
 const styles = {
     display: 'grid',
     gridTemplateColumns: '5fr 1fr 1fr',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingTop: '10px'
 }
 
 const itemNameStyle = {
